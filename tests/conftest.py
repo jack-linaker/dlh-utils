a5c99c0d5c8d53a77053dcd6e64d831bb68eaf2e
Jack Linaker <187010584+jack-linaker@users.noreply.github.com>
2026-01-26 13:52:21 +0000

    refactor: use local spark session for tests
    Removed the use of the spark session function defined in sessions.py.
    Uses a standard local session instead.

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,24 +1,30 @@
-"""Import required libraries"""
+"""Import required libraries."""
+
+from collections.abc import Iterator
+
 import pytest
 from pyspark.sql import SparkSession
-from dlh_utils.sessions import getOrCreateSparkSession
+
 
 @pytest.fixture(scope="session")
-def spark(request):
+def spark() -> Iterator[SparkSession]:
     """Set up the Spark session by using a fixture decorator.
 
-     This will be passed to all the tests by having spark as an
-         input. Being able to define the Spark session in this
-         way is one advantage of Pytest over unittest.
+    This will be passed to all the tests by having spark as an input.
+    Being able to define the Spark session in this way is one advantage
+    of Pytest over unittest.
 
-     Has several options for scope:
-         "function" will build and close it for each function
-         "session" will last for all the tests
+    Has several options for scope:
+        - "function" will build and close it for each function
+        - "session" will last for all the tests
 
-     Generally, "session" will be chosen as the fixture only needs
-         to be set up once and this will make the tests run faster
+    Generally, "session" will be chosen as the fixture only needs to be
+    set up once and this will make the tests run faster
     """
-
-    spark = getOrCreateSparkSession(appName="dlh_utils unit tests", size="small")
-    request.addfinalizer(lambda: spark.stop())
-    return spark
+    spark = (
+        SparkSession.builder.appName("dlh_utils unit tests")
+        .master("local")
+        .getOrCreate()
+    )
+    yield spark
+    spark.stop()
