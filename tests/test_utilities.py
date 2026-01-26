a3443c469297a1f915b8e5a1789eac3664c4d53e
Jack Linaker <187010584+jack-linaker@users.noreply.github.com>
2026-01-26 13:45:18 +0000

    fix: change imports so tests run
    pandas.util.testing is now pandas.testing. IPython.core.display is now
    IPython.display.

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,111 +1,130 @@
-'''
+"""
 Pytesting on Linkage functions.
-'''
+"""
 
-from pyspark.sql import SparkSession
-import pyspark.sql.functions as F
-from pyspark.sql.types import StructType,StructField,StringType,LongType,FloatType,\
-IntegerType, DoubleType, TimestampType
-import pandas as pd
-from pandas.util.testing import assert_frame_equal
-import numpy as np
-import pytest
-from chispa import assert_df_equality
-from dlh_utils.utilities import describe_metrics, value_counts, \
-regex_match, pandas_to_spark, search_files, chunk_list
 import datetime as dt
 import os
+
+import numpy as np
+import pandas as pd
+import pyspark.sql.functions as F
+import pytest
+from chispa import assert_df_equality
+from pandas.testing import assert_frame_equal
+from pyspark.sql import SparkSession
+from pyspark.sql.types import (
+    DoubleType,
+    FloatType,
+    IntegerType,
+    LongType,
+    StringType,
+    StructField,
+    StructType,
+    TimestampType,
+)
+
+from dlh_utils.utilities import (
+    chunk_list,
+    describe_metrics,
+    pandas_to_spark,
+    regex_match,
+    search_files,
+    value_counts,
+)
 
 pytestmark = pytest.mark.usefixtures("spark")
 
 #############################################################################
 
-class TestDescribeMetrics(object):
 
-    def test_expected(self,spark):
+class TestDescribeMetrics(object):
+    def test_expected(self, spark):
         df = spark.createDataFrame(
-          (
-              pd.DataFrame(
-                  {
-                      "colA": ["A", "A", "B", None, "C", "C", "C", None],
-                      "colB": [None, 1, 2, 3, 4, 5, 6, 7]
-                  }
-              )
-          )
-      )
-        result_df = describe_metrics(df, output_mode='pandas')
+            (
+                pd.DataFrame(
+                    {
+                        "colA": ["A", "A", "B", None, "C", "C", "C", None],
+                        "colB": [None, 1, 2, 3, 4, 5, 6, 7],
+                    }
+                )
+            )
+        )
+        result_df = describe_metrics(df, output_mode="pandas")
         intended_df = pd.DataFrame(
-          {
-              "variable": ["colA", "colB"],
-              "type": ["string", "double"],
-              "count": [8, 8],
-              "distinct": [3, 8],
-              "percent_distinct": [37.5, 100],
-              "null": [2, 1],
-              "percent_null": [25, 12.5],
-              "not_null": [6, 7],
-              "percent_not_null": [75, 87.5]
-          }
-      )
+            {
+                "variable": ["colA", "colB"],
+                "type": ["string", "double"],
+                "count": [8, 8],
+                "distinct": [3, 8],
+                "percent_distinct": [37.5, 100],
+                "null": [2, 1],
+                "percent_null": [25, 12.5],
+                "not_null": [6, 7],
+                "percent_not_null": [75, 87.5],
+            }
+        )
         assert_frame_equal(result_df, intended_df)
 
+
 #############################################################################
+
 
 class TestValueCounts(object):
-
-    def test_expected(self,spark):
+    def test_expected(self, spark):
         df = spark.createDataFrame(
-          (
-              pd.DataFrame(
-                  {
-                      "colA": ["A", "A", "B", None, "C", "C", "C", None],
-                      "colB": [None, 1, 2, 3, 3, 5, 5, 6]
-                  }
-              )
-          )
-      )
-        result_df = value_counts(df, limit=6, output_mode='pandas')
+            (
+                pd.DataFrame(
+                    {
+                        "colA": ["A", "A", "B", None, "C", "C", "C", None],
+                        "colB": [None, 1, 2, 3, 3, 5, 5, 6],
+                    }
+                )
+            )
+        )
+        result_df = value_counts(df, limit=6, output_mode="pandas")
         intended_df = pd.DataFrame(
-          {
-              "colA": ["C", None, "A", "B", "", ""],
-              "colA_count": [3, 2, 2, 1, 0, 0],
-              "colB": [3.0, 5.0, 1.0, 2.0, 6.0, np.NaN],
-              "colB_count": [2, 2, 1, 1, 1, 1]
-          }
-      )
+            {
+                "colA": ["C", None, "A", "B", "", ""],
+                "colA_count": [3, 2, 2, 1, 0, 0],
+                "colB": [3.0, 5.0, 1.0, 2.0, 6.0, np.NaN],
+                "colB_count": [2, 2, 1, 1, 1, 1],
+            }
+        )
         assert_frame_equal(result_df, intended_df)
 
 
 #############################################################################
 
-class TestRegexMatch(object):
 
-    def test_expected(self,spark):
+class TestRegexMatch(object):
+    def test_expected(self, spark):
         df = spark.createDataFrame(
-          (
-              pd.DataFrame(
-                  {
-                      "colA": ["abc123_hello", "", None],
-                      "colB": [" abc123_hello", "", None],
-                      "colC": ["123_hello", "", None],
-                      "colD": ["abc_hello", "", None],
-                      "colE": ["abc123hello", "", None],
-                      "colF": ["abc123", "", None],
-                      "colG": ["abc123_hello", "abc123_hello", "abc123_hello"],
-                  }
-              )
-          )
-      )
+            (
+                pd.DataFrame(
+                    {
+                        "colA": ["abc123_hello", "", None],
+                        "colB": [" abc123_hello", "", None],
+                        "colC": ["123_hello", "", None],
+                        "colD": ["abc_hello", "", None],
+                        "colE": ["abc123hello", "", None],
+                        "colF": ["abc123", "", None],
+                        "colG": ["abc123_hello", "abc123_hello", "abc123_hello"],
+                    }
+                )
+            )
+        )
         regex = "^[a-z]*[0-9]+_"
         result = regex_match(df, regex, limit=10000, cut_off=0.0)
-        assert result == ['colA', 'colC', 'colG']
+        assert result == ["colA", "colC", "colG"]
         result = regex_match(df, regex, limit=10000, cut_off=0.6)
-        assert result == ['colG']
+        assert result == ["colG"]
+
 
 #############################################################################
 
+
 class TestPandasToSpark(object):
-    def test_expected(self,spark):
+    def test_expected(self, spark):
         pandas_df = pd.DataFrame(
             {
                 "colDate": ["19000101"],
@@ -113,7 +132,7 @@ class TestPandasToSpark(object):
                 "colBigInt": [1],
                 "colFloat": [1.0],
                 "colBigFloat": [1.0],
-                "colString": ["hello"]
+                "colString": ["hello"],
             }
         )
         pandas_df["colDate"] = pandas_df["colDate"].astype("datetime64[ns]")
@@ -142,22 +161,33 @@ class TestPandasToSpark(object):
 
         assert_df_equality(result_df, intended_df, ignore_row_order=True)
 
+
 #############################################################################
+
 
 class TestSearchFiles(object):
     def test_expected(self, spark):
         path = os.path.dirname(os.path.realpath(__file__))
         result = search_files(path, "import")
-        assert sorted(list(result.keys())) == sorted(['test_formatting.py', 'test_linkage.py', 'test_profiling.py', 'test_standardisation.py', 'test_dataframes.py', 'test_flags.py', 'test_utilities.py', 'conftest.py'])
+        assert sorted(list(result.keys())) == sorted(
+            [
+                "test_formatting.py",
+                "test_linkage.py",
+                "test_profiling.py",
+                "test_standardisation.py",
+                "test_dataframes.py",
+                "test_flags.py",
+                "test_utilities.py",
+                "conftest.py",
+            ]
+        )
+
 
 #############################################################################
+
 
 class TestChunkList(object):
     def test_expected(self, spark):
         data = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         result = chunk_list(data, 4)
-        assert result == [
-          [0, 1, 2, 3],
-          [4, 5, 6, 7],
-          [8, 9, 10]
-        ]
+        assert result == [[0, 1, 2, 3], [4, 5, 6, 7], [8, 9, 10]]
